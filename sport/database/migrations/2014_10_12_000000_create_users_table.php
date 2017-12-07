e104102df68223ccc370938561db9a5a9cb9f4bb
noriane.rozales <noriane.rozales@coding-academy.fr>
2017-12-07 15:27:10 +0100

    merge project start

--- sport/database/migrations/2014_10_12_000000_create_users_table.php
+++ sport/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('user_name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->integer('groupe');
             $table->rememberToken();
             $table->timestamps();
         });
